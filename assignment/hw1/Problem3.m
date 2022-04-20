f36cdb824a0fcc0208ff9aea0f111cec9e9bfe5a
Mahipt <tseng7733@gmail.com>
2022-04-20 00:10:27 -0700

    hw1 problem 3 update

--- assignment/hw1/Problem3.m
+++ assignment/hw1/Problem3.m
@@ -56,103 +56,105 @@ all_y(1,:) = q(2:2:end);
 all_v(1,:) = u(2:2:end); 
 
 f_applyPoint = round(0.75 / (BeamLength / (N - 1)));
-force = 20000; 
+
 
 % Tolerance
 tol = EI / BeamLength^2 * 1e-3;
 
 free_index = [3:(N*2 - 1)]; 
 
-% Time marching scheme
-for c = 2:Nsteps 
-    fprintf('Steps = %f\n', (c - 1) ); 
+force_vs_ymax = zeros(10, 2);
 
-    q = q0; % Guess
-    q_free = q(free_index); 
-    % Newton Raphson
-    err = 10 * tol;
-    while err > tol
-        % Inertia
-        f = M / dt * ( (q-q0) / dt - u );
-        J = M / dt^2;
-        
-        f(f_applyPoint * 2) = f(f_applyPoint * 2) + force; 
+for j = 1:11
+    force = 2000 + 1000 * (j - 1); 
+    fprintf('Force = %f\n', force);
+    % Time marching scheme
+    for c = 2:Nsteps 
+        %fprintf('Steps = %f\n', (c - 1) ); 
 
-        for k = 2:N 
-            xk = q(2 * (k - 1) - 1); 
-            yk = q(2 * (k - 1)); 
-            xkp1 = q(2 * k - 1); 
-            ykp1 = q(2 * k); 
-            dF = gradEs(xk, yk, xkp1, ykp1, deltaL, EA);
-            dJ = hessEs(xk, yk, xkp1, ykp1, deltaL, EA);
+        q = q0; % Guess
+        q_free = q(free_index); 
+        % Newton Raphson
+        err = 10 * tol;
+        while err > tol
+            % Inertia
+            f = M / dt * ( (q-q0) / dt - u );
+            J = M / dt^2;
+            
+            f(f_applyPoint * 2) = f(f_applyPoint * 2) + force; 
 
-            bg = 2 * (k - 1) - 1; % begin
-            fl = 2 * k; % end 
-            f(bg:fl) = f(bg:fl) + dF;
-            J(bg:fl,bg:fl) = J(bg:fl,bg:fl) + dJ;
+            for k = 2:N 
+                xk = q(2 * (k - 1) - 1); 
+                yk = q(2 * (k - 1)); 
+                xkp1 = q(2 * k - 1); 
+                ykp1 = q(2 * k); 
+                dF = gradEs(xk, yk, xkp1, ykp1, deltaL, EA);
+                dJ = hessEs(xk, yk, xkp1, ykp1, deltaL, EA);
+
+                bg = 2 * (k - 1) - 1; % begin
+                fl = 2 * k; % end 
+                f(bg:fl) = f(bg:fl) + dF;
+                J(bg:fl,bg:fl) = J(bg:fl,bg:fl) + dJ;
+            end
+
+
+           
+            for k = 3:N
+                xkm1 = q((k - 2) * 2 - 1); 
+                ykm1 = q((k - 2) * 2); 
+                xk = q((k - 1) * 2 - 1); 
+                yk = q((k - 1) * 2); 
+                xkp1 = q(2 * k - 1); 
+                ykp1 = q(2 * k); 
+                curvature0 = 0;
+                dF = gradEb(xkm1, ykm1, xk, yk, xkp1, ykp1, ...
+                    curvature0, deltaL, EI);
+                dJ = hessEb(xkm1, ykm1, xk, yk, xkp1, ykp1, ...
+                    curvature0, deltaL, EI);
+
+                bg = 2 * (k - 2) - 1; % start 
+                fl = 2 * k; % end
+                f(bg:fl) = f(bg:fl) + dF;
+                J(bg:fl,bg:fl) = J(bg:fl,bg:fl) + dJ;
+            end
+            
+            f = f - W; 
+            % Update (make sure both end-points = 0)
+            f_free = f(free_index); 
+            J_free = J(free_index, free_index); 
+            q_free = q_free - J_free \ f_free; 
+
+            q(free_index) = q_free;
+            
+            err = sum( abs(f(free_index)) );
         end
 
-
-       
-        for k = 3:N
-            xkm1 = q((k - 2) * 2 - 1); 
-            ykm1 = q((k - 2) * 2); 
-            xk = q((k - 1) * 2 - 1); 
-            yk = q((k - 1) * 2); 
-            xkp1 = q(2 * k - 1); 
-            ykp1 = q(2 * k); 
-            curvature0 = 0;
-            dF = gradEb(xkm1, ykm1, xk, yk, xkp1, ykp1, ...
-                curvature0, deltaL, EI);
-            dJ = hessEb(xkm1, ykm1, xk, yk, xkp1, ykp1, ...
-                curvature0, deltaL, EI);
-
-            bg = 2 * (k - 2) - 1; % start 
-            fl = 2 * k; % end
-            f(bg:fl) = f(bg:fl) + dF;
-            J(bg:fl,bg:fl) = J(bg:fl,bg:fl) + dJ;
-        end
+        % Update
+        u = (q - q0) / dt; % Velocity
+        q0 = q; % Old position
         
-        f = f - W; 
-        % Update (make sure both end-points = 0)
-        f_free = f(free_index); 
-        J_free = J(free_index, free_index); 
-        q_free = q_free - J_free \ f_free; 
-
-        q(free_index) = q_free;
         
-        err = sum( abs(f(free_index)) );
+        figure(1);
+        plot(q(1:2:end), q(2:2:end), 'ro-');
+        axis equal
+        drawnow
+        
+        
+        % Store
+        all_mid_y(c,:) = q(2:2:end);
+        all_mid_v(c,:) = u(2:2:end);
     end
-
-    % Update
-    u = (q - q0) / dt; % Velocity
-    q0 = q; % Old position
-    
-    
-    figure(1);
-    plot(q(1:2:end), q(2:2:end), 'ro-');
-    axis equal
-    drawnow
-    
-    
-    % Store
-    all_mid_y(c,:) = q(2:2:end);
-    all_mid_v(c,:) = u(2:2:end);
+    tempt = -1 * all_mid_y(Nsteps,:); 
+    force_vs_ymax(j, 1) = force;
+    force_vs_ymax(j, 2) = max(tempt);
 end
 
 
 figure(2);
-timeArray = (1:Nsteps) * dt;
-plot(timeArray, all_mid_v(), 'k-');
-xlabel('Time, t [sec]');
-ylabel('Velocity of mid-node, v [meter/sec]');
+plot(force_vs_ymax(j,1), force_vs_ymax(j,2), 'k-');
+xlabel('Force, [N]');
+ylabel('Y Max, [Meters]');
 
-
-figure(3); 
-timeArray = (1:Nsteps) * dt;
-plot(timeArray, all_mid_y, 'k-');
-xlabel('Time, t [sec]');
-ylabel('Positio of mid-node, y[meter]');
 
 
 
